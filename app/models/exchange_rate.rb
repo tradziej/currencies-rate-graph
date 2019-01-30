3e004cb4880b6384fd10a20eae45102ead43eb24
Tomasz Radziejewski <tomasz@radziejewski.pl>
2019-01-30 18:28:42 +0100

    Annotate models

--- app/models/exchange_rate.rb
+++ app/models/exchange_rate.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: exchange_rates
+#
+#  id         :bigint(8)        not null, primary key
+#  currency   :string
+#  date       :date
+#  rate       :decimal(12, 5)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class ExchangeRate < ApplicationRecord
 end
--- spec/models/exchange_rate_spec.rb
+++ spec/models/exchange_rate_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: exchange_rates
+#
+#  id         :bigint(8)        not null, primary key
+#  currency   :string
+#  date       :date
+#  rate       :decimal(12, 5)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe ExchangeRate, type: :model do
