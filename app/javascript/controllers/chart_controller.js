cd6b7d0582f2a2d1bbf0006e0962cf96b6dcaf53
Tomasz Radziejewski <tomasz@radziejewski.pl>
2019-02-01 19:14:13 +0100

    Update graph on range change

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "stimulus"
 import "chart.js"
-import { groupBy, values, prop, uniq, map, pipe } from "ramda";
+import { groupBy, values, prop, uniq, map, pipe } from "ramda"
 
 export default class extends Controller {
   static targets = ["ratesChart", "range"]
@@ -10,8 +10,8 @@ export default class extends Controller {
   }
 
   connect() {
-    const rates = JSON.parse(this.element.dataset.exchangeRates)
-    this.loadRatesChart(rates)
+    this.chart
+    this.updateChart()
   }
 
   get range() {
@@ -21,10 +21,129 @@ export default class extends Controller {
   set range(value) {
     this.data.set("range", value)
     this.disableOrEnableButtons()
+    this.loadData()
+  }
+
+  get exchangeRates() {
+    const rates = JSON.parse(this.data.get("exchangeRates"))
+    return rates
+  }
+
+  set exchangeRates(values) {
+    this.data.set("exchangeRates", JSON.stringify(values))
+    this.updateChart()
+  }
+
+  get chart() {
+    if (!this._chart) {
+      const ctx = this.ratesChartTarget.getContext("2d")
+
+      const currencies = ["USD", "AUD", "EUR"]
+
+      const getColor = currency => {
+        const colors = {
+          USD: [30, 158, 218],
+          AUD: [246, 128, 26],
+          EUR: [0, 166, 139]
+        }
+        return colors[currency]
+      }
+
+      const datasets = currencies.map(name => {
+          const color = getColor(name)
+
+          return {
+            label: name,
+            backgroundColor:
+              "rgba(" + color[0] + "," + color[1] + "," + color[2] + ",0.2)",
+            borderColor:
+              "rgba(" + color[0] + "," + color[1] + "," + color[2] + ",1)",
+            pointBackgroundColor:
+              "rgba(" + color[0] + "," + color[1] + "," + color[2] + ",1)",
+            pointBorderColor: "#fff"
+          }
+        }
+      )
+
+      const options = {
+        responsive: true,
+        legend: {
+          position: "bottom"
+        },
+        scales: {
+          xAxes: [
+            {
+              type: "time",
+              time: {
+                parser: "YYYY-MM-DD",
+                unit: "day"
+              },
+              ticks: {
+                autoSkip: true,
+                maxTicksLimit: 12
+              }
+            }
+          ]
+        },
+        elements: {
+          point: {
+            radius: 0,
+            hitRadius: 10,
+            hoverRadius: 5
+          }
+        }
+      }
+
+      this._chart = new Chart(ctx, { type: "line", data: { datasets }, options })
+    }
+    return this._chart
   }
 
   changeRange(event) {
     this.range = event.target.dataset.range
+  }
+
+  loadData() {
+    const url = this.data.get("endpoint")
+    const params = this.getParams()
+
+    const esc = encodeURIComponent
+    const query = Object.keys(params)
+      .map(k => `${esc(k)}=${esc(params[k])}`)
+      .join("&")
+
+    fetch(`${url}?${query}`)
+      .then(response => response.json())
+      .then(this.loaded)
+  }
+
+  loaded = data => {
+    this.exchangeRates = data
+  }
+
+  getParams() {
+    const params = {}
+    const date = new Date()
+
+    switch (this.range) {
+      case "1W":
+        date.setDate(date.getDate() - 7)
+        break
+      case "1M":
+        date.setMonth(date.getMonth() - 1)
+        break
+      case "1Y":
+        date.setMonth(date.getMonth() - 12)
+        break
+      case "3Y":
+        date.setFullYear(date.getFullYear() - 3)
+        break
+      case "5Y":
+        date.setFullYear(date.getFullYear() - 5)
+        break
+    }
+    params["from_date"] = date.toISOString().slice(0, 10)
+    return params
   }
 
   disableOrEnableButtons() {
@@ -37,76 +156,30 @@ export default class extends Controller {
     }, this)
   }
 
-  loadRatesChart(rates) {
-    const ctx = this.ratesChartTarget.getContext("2d")
+  updateChart() {
+    const data = this.exchangeRates
 
     const byCurrency = groupBy(prop("currency"))
-    const groupedCurrencies = byCurrency(rates)
-    const currencies = ["USD", "AUD", "EUR"]
+    const groupedCurrencies = byCurrency(data)
 
-    const uniqueDates = pipe(map(prop("date")), uniq)
-    const labels = uniqueDates(rates)
+    const uniqueDates = pipe(
+      map(prop("date")),
+      uniq
+    )
+    const labels = uniqueDates(data)
 
-    const getColor = currency => {
-      const colors = {
-        "USD": [30, 158, 218],
-        "AUD": [246, 128, 26],
-        "EUR": [0, 166, 139],
-      }
-      return colors[currency]
-    }
-
-    const datasets = values(groupedCurrencies).map((currencyRates, index) => {
-      const currencyName = currencies[index]
-      const color = getColor(currencyName)
-
-      return {
-        label: currencyName,
-        backgroundColor:
-          "rgba(" + color[0] + "," + color[1] + "," + color[2] + ",0.2)",
-        borderColor:
-          "rgba(" + color[0] + "," + color[1] + "," + color[2] + ",1)",
-        pointBackgroundColor:
-          "rgba(" + color[0] + "," + color[1] + "," + color[2] + ",1)",
-        pointBorderColor: "#fff",
-        data: currencyRates.map(rate => parseFloat(rate.rate))
-      }
+    const datasets = values(groupedCurrencies).map(currencyRates => {
+      return currencyRates.map(rate => parseFloat(rate.rate))
     })
 
-    const data = {
-      datasets: datasets,
-      labels: labels
-    }
+    this.chart.data.labels = labels
 
-    const options = {
-      responsive: true,
-      legend: {
-        position: "bottom"
-      },
-      scales: {
-        xAxes: [
-          {
-            type: "time",
-            time: {
-              parser: "YYYY-MM-DD",
-              unit: "day"
-            },
-            ticks: {
-              autoSkip: true,
-              maxTicksLimit: 12
-            }
-          }
-        ]
-      },
-      elements: {
-        point: {
-          radius: 0,
-          hitRadius: 10,
-          hoverRadius: 5
-        }
-      }
-    }
+    this.chart.data.datasets.forEach((dataset, index) => {
+      dataset.data = datasets[index]
+    })
 
-    return new Chart(ctx, { type: "line", data, options })
+    this.chart.update({
+      duration: 0
+    })
   }
 }
